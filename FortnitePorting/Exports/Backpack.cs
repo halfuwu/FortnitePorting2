481d775aa816b588cfdf22dd6285154420c6a414
halfuwu <maxman1346@gmail.com>
2022-05-01 15:09:32 -0400

    backpack export support

---
+++ FortnitePorting/Exports/Backpack.cs
@@ -0,0 +1,33 @@
+﻿using CUE4Parse.UE4.Assets.Exports;
+using CUE4Parse.UE4.Objects.Core.i18N;
+using static FortnitePorting.FortnitePorting;
+
+namespace FortnitePorting.Exports;
+
+public static class Backpack
+{
+    public static ExportFile? Export(string input)
+    {
+        var path = $"FortniteGame/Content/Athena/Items/Cosmetics/Characters/{input}.{input}";
+        if (!input.StartsWith("BID_")) 
+            path = Benbot.GetCosmeticPath(input, "AthenaBackpack");
+
+
+        if (Provider.TryLoadObject(path, out var backpack))
+        {
+            var export = new ExportFile();
+            export.name = backpack.Get<FText>("DisplayName").Text;
+            
+            var parts = backpack.Get<UObject[]>("CharacterParts");
+            export.baseStyle = new List<ExportPart>();
+            AssetHelpers.ExportCharacterParts(parts, export.baseStyle);
+
+            var styles = backpack.GetOrDefault("ItemVariants", Array.Empty<UObject>());
+            AssetHelpers.ExportStyles(styles, ref export);
+
+            return export;
+        }
+
+        return null;
+    }
+}
