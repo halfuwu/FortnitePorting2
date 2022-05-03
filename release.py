31dae31dd6a65de7d8fe3d24530981c06f28be87
halfuwu <maxman1346@gmail.com>
2022-05-02 20:39:08 -0400

    and remove zip (for now)

--- release.py
+++ release.py
@@ -14,15 +14,12 @@ for file in glob.glob(publishPath + "**"):
 
 os.system(f'dotnet publish -c Release -r win-x64 -o "{publishPath}" --self-contained -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -p:DebugType=None -p:DebugSymbols=false')
 
-with zipfile.ZipFile('Release/FortnitePortingAddon.zip', 'w') as addonZip:
-    addonZip.write('FortnitePortingAddon.py', 'FortnitePorting/FortnitePortingAddon.py')
-
 with zipfile.ZipFile('Release/FortnitePorting.zip', 'w') as mainZip:
     for file in glob.glob(publishPath + "**"):
         mainZip.write(file, os.path.basename(file))
     mainZip.write('FPShader.blend')
     mainZip.write('config.json')
-    mainZip.write('Release/FortnitePortingAddon.zip', 'FortnitePortingAddon.zip')
+    mainZip.write('FortnitePortingAddon.py')
 
 os.remove('Release/FortnitePortingAddon.zip')
 
